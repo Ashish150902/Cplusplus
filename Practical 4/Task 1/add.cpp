bddff842363f5903e37eb7c33d6f820de82abba6
Ashish150902 <91263837+Ashish150902@users.noreply.github.com>
2021-11-24 20:42:58 +0530

    Add files via upload

---
+++ Practical 4/Task 1/add.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+namespace First
+{
+	int add(int a,int b)
+	{
+		return (a+b);
+	}
+}
+namespace Second
+{
+	float add(float a,float b)
+	{
+		return (a+b);
+	}
+}
+int main()
+{
+	int a= First::add(2,9);
+	float b=Second::add(5.9,6.3);
+	cout<<a<<"\n"<<b;
+}
